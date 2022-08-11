24f9c41c55240b067e8c16cc120904dd61e3a9ea
Arif Kalluru <arifkalluru@gmail.com>
2022-08-11 11:16:39 +0530

    Add 141. Linked List Cycle

---
+++ 141-linked-list-cycle.cpp
@@ -0,0 +1,21 @@
+// Source : https://leetcode.com/problems/linked-list-cycle/
+
+class Solution {
+public:
+  bool hasCycle(ListNode *head) {
+    if (head == nullptr)
+      return false;
+
+    ListNode *fast = head;
+    ListNode *slow = head;
+
+    while (fast != nullptr && fast->next != nullptr) {
+      fast = fast->next->next;
+      slow = slow->next;
+
+      if (fast == slow)
+        return true;
+    }
+    return false;
+  }
+};
