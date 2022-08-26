b7f5486a5546f1dafe501407daf0e5990f353e09
Arif Kalluru <arifkalluru@gmail.com>
2022-08-26 12:20:41 +0530

    Add 100. Same Tree

---
+++ 100-same-tree.cpp
@@ -0,0 +1,14 @@
+// Source : https://leetcode.com/problems/same-tree/
+
+class Solution {
+public:
+  bool isSameTree(TreeNode *p, TreeNode *q) {
+    if (!p && !q)
+      return true;
+    if (p == nullptr || q == nullptr)
+      return false;
+    if (p->val != q->val)
+      return false;
+    return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+  }
+};
